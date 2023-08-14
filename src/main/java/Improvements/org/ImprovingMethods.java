cb1e938fbb60a25ed63ee305683955794f95c8b8
Mohammed Al-Eiadeh <93108547+Mohammed-Ryiad-Eiadeh@users.noreply.github.com>
2023-08-14 14:07:30 +0300

    Update ImprovingMethods.java

--- src/main/java/Improvements/org/ImprovingMethods.java
+++ src/main/java/Improvements/org/ImprovingMethods.java
@@ -43,21 +43,20 @@ public final class ImprovingMethods extends Feasibility {
      * @return The offspring that is produced by applying Xor operation on randomly selected two parents
      */
     public double[] DoXor(double[][] solutions, FitnessFunction FN) {
-        var parent01 = Arrays.copyOf(solutions[new Random().nextInt(solutions.length)], solutions[0].length);
-        var parent02 = Arrays.copyOf(solutions[new Random().nextInt(solutions.length)], solutions[0].length);
-        var offspring = new double[parent01.length];
-
-        for (var i = 0; i < parent01.length ; i++)
+        double[] parent01 = Arrays.copyOf(solutions[new Random().nextInt(solutions.length)], solutions[0].length);
+        double[] parent02 = Arrays.copyOf(solutions[new Random().nextInt(solutions.length)], solutions[0].length);
+        double[] offspring = new double[parent01.length];
+        for (int i = 0; i < parent01.length ; i++) {
             offspring[i] = parent01[i] == parent02[i] ? 0.0 : 1.0;
-
-        var FeasibleOffspring = GuaranteeFeasibility(offspring);
-        var OffspringFit = FN.EvaluateSingleSolution(FeasibleOffspring);
-        if (OffspringFit < FN.EvaluateSingleSolution(parent01))
+        }
+        double[] FeasibleOffspring = GuaranteeFeasibility(offspring);
+        int OffspringFit = FN.EvaluateSingleSolution(FeasibleOffspring);
+        if (OffspringFit < FN.EvaluateSingleSolution(parent01)) {
             FeasibleOffspring = parent01;
-
-        if (OffspringFit < FN.EvaluateSingleSolution(parent02))
+        }
+        if (OffspringFit < FN.EvaluateSingleSolution(parent02)) {
             FeasibleOffspring = parent02;
-
+        }
         return FeasibleOffspring;
     }
 
@@ -65,17 +64,17 @@ public final class ImprovingMethods extends Feasibility {
      * @return the best offspring that is produced by applying single point crossover on randomly selected two parents
      */
     public double[] DoSinglePointCrossOver(double[][] Solutions, FitnessFunction FN) {
-        var parent1 = new double[Solutions[0].length];
+        double[] parent1 = new double[Solutions[0].length];
         System.arraycopy(Solutions[new Random().nextInt(Solutions.length)], 0, parent1, 0, parent1.length);
-        var parent2 = new double[Solutions[0].length];
+        double[] parent2 = new double[Solutions[0].length];
         System.arraycopy(Solutions[new Random().nextInt(Solutions.length)], 0, parent2, 0, parent2.length);
 
-        var offspring1 = new double[parent1.length];
-        var offspring2 = new double[parent2.length];
+        double[] offspring1 = new double[parent1.length];
+        double[] offspring2 = new double[parent2.length];
 
-        var CrossPoint = new Random().nextInt(parent1.length);
-        var holder1 = new double[CrossPoint];
-        var holder2 = new double[parent1.length - holder1.length];
+        int CrossPoint = new Random().nextInt(parent1.length);
+        double[] holder1 = new double[CrossPoint];
+        double[] holder2 = new double[parent1.length - holder1.length];
 
         System.arraycopy(parent1, 0, holder1, 0, holder1.length);
         System.arraycopy(parent2, 0, offspring1, 0, holder1.length);
@@ -84,11 +83,10 @@ public final class ImprovingMethods extends Feasibility {
         System.arraycopy(parent2, holder1.length, offspring2, 0, holder2.length);
         System.arraycopy(holder2, 0, offspring1, holder1.length, holder2.length);
 
-        var feasible1 = GuaranteeFeasibility(offspring1);
-        var feasible2 = GuaranteeFeasibility(offspring2);
-        var x1 = FN.EvaluateSingleSolution(feasible1) > FN.EvaluateSingleSolution(parent1) ? feasible1 : parent1;
-        var x2 = FN.EvaluateSingleSolution(feasible2) > FN.EvaluateSingleSolution(parent2) ? feasible2 : parent2;
-
+        double[] feasible1 = GuaranteeFeasibility(offspring1);
+        double[] feasible2 = GuaranteeFeasibility(offspring2);
+        double[] x1 = FN.EvaluateSingleSolution(feasible1) > FN.EvaluateSingleSolution(parent1) ? feasible1 : parent1;
+        double[] x2 = FN.EvaluateSingleSolution(feasible2) > FN.EvaluateSingleSolution(parent2) ? feasible2 : parent2;
         return FN.EvaluateSingleSolution(x1) > FN.EvaluateSingleSolution(x2) ? x1 : x2;
     }
 }
